ef3bcf2f7fec4ba58cf2d2121b5f5d2970760abc
Laura Wynter <lauracalderininblue5@gmail.com>
2022-08-23 14:33:12 +0200

    Solving errors Dom

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,109 +1,83 @@
-import React from 'react';
-import { useState, useEffect } from "react";
-import useAuth from "./useAuth";
-import Player from "./Player";
-import TrackSearchResult from "./TrackSearchResult";
-import CssBaseline from '@mui/material/CssBaseline';
-import { Box } from '@mui/material';
+import React, { useState, useEffect }  from 'react';
+import useAuth from './useAuth';
 import Container from '@mui/material/Container';
 import FormControl from '@mui/material/FormControl';
-import SpotifyWebApi from "spotify-web-api-node";
-import axios from "axios";
-
+import SpotifyWebApi from 'spotify-web-api-node';
+import TrackSearchResults from './TrackSearchResult';
+import Player from './Player';
+import axios from 'axios';
 const spotifyApi = new SpotifyWebApi({
-  clientId: "8b945ef10ea24755b83ac50cede405a0",
+    clientId: process.env.REACT_APP_CLIENT_ID,
 })
-
-export default function Dashboard({ code }) {
-  const accessToken = useAuth(code)
-  const [search, setSearch] = useState("")
-  const [searchResults, setSearchResults] = useState([])
-  const [playingTrack, setPlayingTrack] = useState()
-  const [lyrics, setLyrics] = useState("")
-
-  function chooseTrack(track) {
-    setPlayingTrack(track)
-    setSearch("")
-    setLyrics("")
-  }
-
-  useEffect(() => {
-    if (!playingTrack) return
-
-    axios
-      .get("http://localhost:3001/lyrics", {
-        params: {
-          track: playingTrack.title,
-          artist: playingTrack.artist,
-        },
-      })
-      .then(res => {
-        setLyrics(res.data.lyrics)
-      })
-  }, [playingTrack])
-
-  useEffect(() => {
-    if (!accessToken) return
-    spotifyApi.setAccessToken(accessToken)
-  }, [accessToken])
-
-  useEffect(() => {
-    if (!search) return setSearchResults([])
-    if (!accessToken) return
-
-    let cancel = false
-    spotifyApi.searchTracks(search).then(res => {
-      if (cancel) return
-      setSearchResults(
-        res.body.tracks.items.map(track => {
-          const smallestAlbumImage = track.album.images.reduce(
-            (smallest, image) => {
-              if (image.height < smallest.height) return image
-              return smallest
-            },
-            track.album.images[0]
-          )
-
-          return {
-            artist: track.artists[0].name,
-            title: track.name,
-            uri: track.uri,
-            albumUrl: smallestAlbumImage.url,
-          }
+export default function Dashboard({ code }){
+    const accessToken = useAuth(code)
+    const [search, setSearch] = useState('')
+    const [searchResults, setSearchResults] = useState([])
+    const [playingTrack, setPlayingTrack] = useState()
+    const [lyrics, setLyrics] = useState('')
+    function chooseTrack(track) {
+        setPlayingTrack(track)
+        setSearch('')
+        setLyrics('')
+    }
+    useEffect(() => {
+        if(!playingTrack) return
+        axios.get('http://localhost:3001/lyrics', {
+            params: {
+                track: playingTrack.title,
+                artist: playingTrack.artist
+            }
         })
-      )
-    })
-
-    return () => (cancel = true)
-  }, [search, accessToken])
-
-  return (
-    <Container className="d-flex flex-column py-2" style={{ height: "100vh" }}>
-      <CssBaseline>
-        <FormControl
-          type="search"
-          placeholder="Search Songs/Artists"
-          value={search}
-          onChange={e => setSearch(e.target.value)}
-        />
-        <Box className="flex-grow-1 my-2" style={{ overflowY: "auto" }}>
-          {searchResults.map(track => (
-            <TrackSearchResult
-              track={track}
-              key={track.uri}
-              chooseTrack={chooseTrack}
-            />
-          ))}
-          {searchResults.length === 0 && (
-            <div className="text-center" style={{ whiteSpace: "pre" }}>
-              {lyrics}
-            </div>
-          )}
-        </Box>
-        <Box>
-          <Player accessToken={accessToken} trackUri={playingTrack?.uri} />
-        </Box>
-      </CssBaseline>
-    </Container>
-  )
+        .then(res => {
+            setLyrics(res.data.lyrics)
+        })
+    }, [playingTrack])
+    useEffect(() => {
+        if(!accessToken) return
+        spotifyApi.setAccessToken(accessToken)
+    }, [accessToken])
+    useEffect(() => {
+        if(!search) return setSearchResults([])
+        if(!accessToken) return
+        let cancel = false
+        spotifyApi.searchTracks(search)
+        .then(res =>{
+            if(cancel) return
+            setSearchResults(res.body.tracks.items.map(track => {
+                const smallestAlbumImage = track.album.images.reduce((smallest, image) => {
+                    if(image.height < smallest.height) return image
+                    return smallest
+                }, track.album.images[0])
+                return{
+                    artist: track.artists[0].name,
+                    title: track.name,
+                    uri: track.uri,
+                    albumUrl: smallestAlbumImage.url
+                }
+            }))
+        })
+        return () => cancel = true
+    }, [search, accessToken])
+    return(
+        <Container className='d-flex flex-column py-2' style={{ height: '100vh' }}>
+            <FormControl type='search' placeholder='Search Songs/Artists' value={search} onChange={e => setSearch(e.target.value)}/>
+            <section className='flex-grow-1 my-2' style={{ overflow: 'auto' }}>
+            {searchResults.map(track => (
+                <TrackSearchResults track={track} key={track.uri} chooseTrack={chooseTrack}/>
+            ) )}
+            {searchResults.length === 0 && (
+                <section className="text-center" style={{ whiteSpace: "pre" }}>
+                    {lyrics}
+                </section>
+            )}
+            </section>
+            <section><Player accessToken={ accessToken } trackUri={playingTrack?.uri}/></section>
+        </Container>
+    )
 }
+
+
+
+
+
+
