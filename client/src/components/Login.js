0b4acce99fdf1f7f1b16f2fcd614d8cd6c12d46a
nuria-barea <nuriab@gmail.com>
2022-08-23 16:11:06 +0200

    #6 solving errors in API Spotify

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -2,7 +2,7 @@ import React from "react";
 import Container from '@mui/material/Container';
 
 const AUTH_URL =
-  "https://accounts.spotify.com/authorize?client_id=e31cc37bef3942d0abe0387329f1dfb3&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state"
+  "https://accounts.spotify.com/authorize?client_id=fc64ddc1f9324f2da48683f19d84bf9c&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state"
 
   export default function Login() {
     return (
@@ -10,9 +10,11 @@ const AUTH_URL =
         className="d-flex justify-content-center align-items-center"
         style={{ minHeight: "100vh" }}
       >
-        <a className="btn btn-success btn-lg" href={AUTH_URL}>
+        <button style={{backgroundcolor:"green"}}>
+        <a type  href={AUTH_URL}>
           Login With Spotify
         </a>
+        </button>
       </Container>
     )
   }
